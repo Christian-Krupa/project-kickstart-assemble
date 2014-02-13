2073022f418eee7c0aaf998d9fecf1bb76b0d3e0
Stephan Zavodny <info@rastersysteme.de>
2014-02-13 09:16:03 +0100

    added centering-mixin | added globbing pattern for assemble pages | added svgmin-options because of problems on windows machines

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
 						dest: 'build/',
 						expand: true,
 						flatten: true,
-						src: ['*.hbs']
+						src: ['**/*.hbs']
 					}
 				]
 			},
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
 						dest: 'dist/',
 						expand: true,
 						flatten: true,
-						src: ['*.hbs']
+						src: ['**/*.hbs']
 					}
 				]
 			}
@@ -469,6 +469,41 @@ module.exports = function(grunt) {
 		
 		// Configuration for optimizing SVG-files
 		svgmin: {
+			options: {
+				 plugins: [
+					{ cleanupAttrs: true },
+					{ cleanupEnableBackground: true },
+					{ cleanupIDs: true },
+					{ cleanupNumericValues: true },
+					{ collapseGroups: true },
+					{ convertColors: true },
+					{ convertPathData: true },
+					{ convertShapeToPath: true },
+					{ convertStyleToAttrs: true },
+					{ convertTransform: true },
+					{ mergePaths: true },
+					{ moveElemsAttrsToGroup: true },
+					{ moveGroupAttrsToElems: true },
+					{ removeComments: true },
+					{ removeDoctype: true },
+					{ removeEditorsNSData: true },
+					{ removeEmptyAttrs: true },
+					{ removeEmptyContainers: true },
+					{ removeEmptyText: true },
+					{ removeHiddenElems: true },
+					{ removeMetadata: true },
+					{ removeNonInheritableGroupAttrs: true },
+					{ removeRasterImages: true },
+					{ removeTitle: true },
+					{ removeUnknownsAndDefaults: true },
+					{ removeUnusedNS: true },
+					{ removeUselessStrokeAndFill: false }, // Enabling this may cause small details to be removed
+					{ removeViewBox: false }, // Keep the viewBox because that's where illustrator hides the SVG dimensions
+					{ removeXMLProcInst: false }, // Enabling this breaks grunticon because it removes the XML header
+					{ sortAttrs: true },
+					{ transformsWithOnePath: false } // Enabling this breaks Illustrator SVGs with complex text
+				]
+			},
 			all: {
 				files: [
 					{
